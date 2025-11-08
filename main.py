380970c4d7996c23fa9c7879bd15b0505ac17ad8
rishin <rishinrnew2020@gmail.com>
2025-11-08 18:57:16 +0530

    start page random function removed

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import string
-import random
 from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get('/')
 async def index():
-    mystr = ''.join(random.choices(string.ascii_lowercase, k = 5))
-    return {'data': mystr}
+    return {'data': 'Hello World!'}
 
 @app.get('/items/{item_id}')
 async def read(item_id: int):
